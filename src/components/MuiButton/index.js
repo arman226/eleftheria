e5fed34b4530a5fd839eb3ef8b9dab565d6ebb53
I Am Kaz <kazuyasato01@yahoo.com>
2022-11-12 22:59:43 +0800

    delete LoginPage hehe

--- src/components/MuiButton/index.js
+++ src/components/MuiButton/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 
-const MuiButton = ({ title }) => {
-  return (
-    <Button variant="contained" color="red" disableElevation>
-      {title}
-    </Button>
-  );
+const MuiButton = ({ title, ...inputProps }) => {
+  return <Button {...inputProps}>{title}</Button>;
 };
 
 export default MuiButton;
